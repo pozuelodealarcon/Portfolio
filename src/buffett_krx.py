be80724ce7be9db070166b9d3f3948d3afda9650
Hyungsuk Choi <157815305+pozuelodealarcon@users.noreply.github.com>
2025-07-01 14:11:48 +0000

    update score logic

--- src/buffett_krx.py
+++ src/buffett_krx.py
@@ -59,7 +59,7 @@ lee_kw_list = [ #2025 이재명 정부 예상 수혜주
 ]
 
 country = 'KR'
-limit=10 # 250 requests/day
+limit=200 # 250 requests/day
 sp500 = True
 
 #########################################################

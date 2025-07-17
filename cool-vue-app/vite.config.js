84595487175d9c5865006b0439def4c801a81ec6
Hyungsuk Choi <157815305+pozuelodealarcon@users.noreply.github.com>
2025-07-17 15:05:50 +0000

    vue

--- cool-vue-app/vite.config.js
+++ cool-vue-app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/cool-vue-app/',  // 깃허브 리포지토리 이름, 예: '/cool-vue-app/'
+  base: '/Portfolio/',
   plugins: [vue()]
 })
